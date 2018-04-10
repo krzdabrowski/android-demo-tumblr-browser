53b099504027eff23138d016450b6543a62fa1b3
krzysiek <krzdabrowski95@gmail.com>
2018-04-10 19:26:46 +0200

    Remove empty Settings from menu

--- app/src/main/java/com/example/trubul/tumblrbrowser/MainActivity.java
+++ app/src/main/java/com/example/trubul/tumblrbrowser/MainActivity.java
@@ -66,10 +66,6 @@ public class MainActivity extends BaseActivity implements DataJSON.AdapterCallba
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         if (id == R.id.action_search) {
             Intent intent = new Intent(this, SearchActivity.class);
             startActivity(intent);
